7faef4834f81830498fbd57eb71cf1cae70860ff
Sourav Kumar Parida <111356351+Sourav-Parida@users.noreply.github.com>
2022-09-03 18:08:31 +0530

    Add files via upload

---
+++ E_While-1-10-times/1_print-10-times.c
@@ -0,0 +1,15 @@
+//1. Write a program to print MySirG n times on the screen.
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    printf("Program to print MySirG n times on the screen.");
+    printf("\n\n");
+    int i = 1;
+    while(i<=10){
+        printf("MySirG  ");
+        i=i+1;
+    }
+getch();
+}
